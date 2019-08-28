5b229ed4f6b1f7275fac0037302d9759c43f459b
Sebastian Willenbrink <sebastian.willenbrink@tum.de>
2019-08-28 21:32:46 +0200

    Only run game when compilation succeded

--- src/ocaml/run.sh
+++ src/ocaml/run.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-dune build ./main.exe
-_build/default/main.exe -datadir "/opt/warzone2100/data" -autogame
+dune build ./main.exe && _build/default/main.exe -datadir "/opt/warzone2100/data" -autogame
